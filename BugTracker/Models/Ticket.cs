9aae5210c45955338eb89b18d2b8ba8797533226
Nhi Ngo <nhingo6250@gmail.com>
2020-07-27 18:53:03 -0400

    Added login and register pages using Identity

---
+++ BugTracker/Models/Ticket.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTracker.Models
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+        public int ProjectId {get; set;}
+        public string Title { get; set; }
+        public DateTime DateCreated { get; set;}
+        public string Description { get; set; }
+    }
+}
